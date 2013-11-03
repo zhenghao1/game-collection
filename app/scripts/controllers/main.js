530c748ed107b8bd345e89c87fda5e04b62349b5
Mark Huang <mark.huang@ebalu.com>
2013-11-04 03:41:04 +0800

    Step 05
    Added consoles through XHR Requests and image links

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,91 +2,9 @@
 
 var Ctrl = angular.module('gameCollectionApp.controllers', []);
 
-Ctrl.controller('GameListCtrl', function ($scope) {
-    $scope.games = [
-        {
-            "id": "18971",
-            "name": {
-                "us": "Hohokum",
-                "uk": "Hohokum"
-            },
-            "platform": {
-                "id": "5",
-                "name": "PS3"
-            },
-            "updated_date": "2013-09-19T14:43:30.00+0000",
-            "url": "http://www.gamesradar.com/ps3/hohokum",
-            "images": {
-                "thumbnail": "http://static.gamesradar.com/images/empty_01_logo.jpg"
-            }
-        },
-        {
-            "id": "18871",
-            "name": {
-                "us": "Narco Terror",
-                "uk": "Narco Terror"
-            },
-            "platform": {
-                "id": "5",
-                "name": "PS3"
-            },
-            "release_date": {
-                "us": "2013-07-30T13:37:24.00+0000"
-            },
-            "updated_date": "2013-07-30T13:37:27.00+0000",
-            "url": "http://www.gamesradar.com/ps3/narco-terror",
-            "images": {
-                "thumbnail": "http://static.gamesradar.com/images/empty_01_logo.jpg"
-            }
-        },
-        {
-            "id": "18809",
-            "name": {
-                "us": "LEGO Movie Game",
-                "uk": "LEGO Movie Game"
-            },
-            "platform": {
-                "id": "5",
-                "name": "PS3"
-            },
-            "updated_date": "2013-07-16T12:59:13.00+0000",
-            "url": "http://www.gamesradar.com/ps3/lego-movie-game",
-            "images": {
-                "thumbnail": "http://static.gamesradar.com/images/empty_01_logo.jpg"
-            }
-        },
-        {
-            "id": "18829",
-            "name": {
-                "us": "Final Fantasy I",
-                "uk": "Final Fantasy I"
-            },
-            "platform": {
-                "id": "5",
-                "name": "PS3"
-            },
-            "updated_date": "1993-11-06T12:59:13.00+0000",
-            "url": "http://www.gamesradar.com/ps3/lego-movie-game",
-            "images": {
-                "thumbnail": "http://static.gamesradar.com/images/empty_01_logo.jpg"
-            }
-        },
-        {
-            "id": "18839",
-            "name": {
-                "us": "Final Fantasy II",
-                "uk": "Final Fantasy II"
-            },
-            "platform": {
-                "id": "5",
-                "name": "PS3"
-            },
-            "updated_date": "1994-03-16T12:59:13.00+0000",
-            "url": "http://www.gamesradar.com/ps3/lego-movie-game",
-            "images": {
-                "thumbnail": "http://static.gamesradar.com/images/empty_01_logo.jpg"
-            }
-        }
-    ];
-    $scope.orderProp = "name.us";
-});
+Ctrl.controller('GameListCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('http://localhost:6543/platform').success(function(data) {
+        $scope.platforms = data;
+    });
+    $scope.orderProp = "name";
+}]);
